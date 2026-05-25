5f9c3a000d0a1e77b31b02d0e951078c928abeec
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2026-05-25 22:54:43 +0700

    feat: implement TodoList and Todo classes with fetch functionality

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log('Hello there');
+class TodoList {
+  constructor() {
+    this.todos = [];
+  }
+
+  addTodo(newTodo) {
+    this.todo.push(newTodo);
+  }
+
+  removeTodo(id) {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+  }
+}
+class Todo {
+  constructor(id, todo, completed) {
+    this.id = id;
+    this.todo = todo;
+    this.completed = completed;
+  }
+
+  toggleTodo() {
+    this.completed = !this.completed;
+  }
+
+  editTodo(newTodo) {
+    this.todo = newTodo;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', async function () {
+  const data = await fetchData();
+  const todos = data.todos;
+});
+
+async function fetchData() {
+  try {
+    const res = await fetch('https://dummyjson.com/todo');
+    if (!res.ok) {
+      throw new Error(`Status: ${res.status}`);
+    }
+    const data = await res.json();
+
+    return data;
+  } catch (err) {
+    return err;
+  }
+}
